1b1e286efdbb7792fac1b764783334ad82e0d20f
Arisya Yasak <124141390+NurArisya@users.noreply.github.com>
2025-06-28 11:09:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import Home from './components/Home';
